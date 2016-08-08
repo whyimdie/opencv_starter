085bb8152a5cb63fd02d04b610df480f351aab94
ntrranf108 <ntranf108@gmail.com>
2016-08-08 17:16:49 +0700

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -1,4 +1,3 @@
-// Ghost effect video
 #include <iostream>
 #include <cv.h>   // headers
 #include <highgui.h>
@@ -15,6 +14,10 @@ int main(  int argc, char** argv ) {
 	int frameCount;
 	Mat frame;
 	Mat preFrame;
+	Mat grayFrame;
+	Mat frameDiff;
+	Mat preGrayFrame;
+	int pix;
 
 
 
@@ -22,22 +25,46 @@ int main(  int argc, char** argv ) {
 	if(!cap.isOpened()){return -1;}
 	cap >> frame;
 
+	cout <<"Frame columns: " <<frame.cols <<endl;			//look at frame cols and rows to DRAW
+	cout <<"Frame rows: " <<frame.rows <<endl;
+	
+	//in this case, cols = 640 and rows = 480 (check the cols and rows when run cmd, CHECK AND CHANGE BELOW);
+	int frameColumn = 640;
+	int frameRow = 480; 
+
 	namedWindow("Original",CV_WINDOW_AUTOSIZE);
 	namedWindow("Lane Detection",CV_WINDOW_AUTOSIZE);
 
 	for (; ; frameCount++){
 		cap >> frame;
 
-		rectangle(frame,Rect(270,10,100,100),Scalar(0,255,255),2,8,0);
-
-
-
-		imshow("Original",preFrame);
+		rectangle(frame,Rect(270,0,100,480),Scalar(0,255,255),2,8,0);
+		
+		cvtColor(frame,grayFrame,CV_RGB2GRAY);				//make the frame to gray, take effect in gray scale;
+		frame.copyTo(preGrayFrame);
+		
+		int x,y;											//x run to cols, y run to rows
+		for ( ; x < preGrayFrame.cols; x++) {
+		preGrayFrame.at<uchar> (x,y) 
+	
+		
+		if (preGrayFrame.at<uchar>(x,y))
+		
+		
+		
 		frame.copyTo(preFrame);
+		absdiff(preFrame,frame,frameDiff);
+		
+		
+		
+
+
+
+		imshow("Original",frame);
+//		frame.copyTo(frame);
 
 		if(waitKey(20) >= 0) break;
 	}
     return 0;
 
 }
-
